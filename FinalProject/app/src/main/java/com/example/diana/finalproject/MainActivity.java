b94991aa2e226e6b25e1a01488bc3db9af8e78fc
Lilian Patiño Ortiz <lilly.0608@gmail.com>
2015-09-29 08:17:14 -0500

    Login, Register and Forgotten Password
    button style and functionality missing

--- FinalProject/app/src/main/java/com/example/diana/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/diana/finalproject/MainActivity.java
@@ -1,37 +1,79 @@
 package com.example.diana.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button bLogin;
+    EditText eMatricula, eContrasena;
+    TextView tvLinkRegistro, tvOlvidoPass;
+    /*
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+            int id = item.getItemId();
+            if (id == R.id.tvLinkRegistro)
+            {
+                Log.d("CREATION", "tvLinkRegistro is being executed!");
+                finish();
+                Intent i = new Intent(this,Registro.class);
+                startActivity(i);
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i("CREATION", "holiwis");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eMatricula = (EditText)findViewById(R.id.eMatricula);
+        eContrasena = (EditText)findViewById(R.id.eContrasena);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvLinkRegistro = (TextView) findViewById(R.id.tvLinkRegistro);
+        tvOlvidoPass = (TextView) findViewById(R.id.tvOlvidoPass);
+
+        bLogin.setOnClickListener(this);
+        tvLinkRegistro.setOnClickListener(this);
+        tvOlvidoPass.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    //@Override
+    public void onClick(View v)
+    {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch(v.getId())
+        {
+            case R.id.bLogin:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                Log.d("CREATION", "bLogin is being executed!");
+
+                break;
+
+            case R.id.tvLinkRegistro:
+
+                Log.d("CREATION", "tvLinkRegistro is being executed!");
+                finish();
+                Intent i = new Intent(this,Registro.class);
+                startActivity(i);
+                break;
+
+            case R.id.tvOlvidoPass:
+                Log.d("CREATION", "tvOlvidoPass is being executed!");
+                finish();
+                i = new Intent(this,Recupera.class);
+                startActivity(i);
+                break;
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
