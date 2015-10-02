e2166486b653624c25b55943e3f1389fe0197421
andyMonterrubio <andy.mont18@gmail.com>
2015-10-02 00:20:43 -0500

    Alineacion de botones e inputs

--- FinalProject/app/src/main/java/com/example/diana/finalproject/Editar_Perfil.java
+++ FinalProject/app/src/main/java/com/example/diana/finalproject/Editar_Perfil.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
-;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
 
 /**
  * Created by Andrea on 30/09/2015.
  */
-public class Editar_Perfil extends AppCompatActivity {
+public class Editar_Perfil extends AppCompatActivity implements View.OnClickListener {
+    Button bCancel, bSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editperfil); // edit perfil
+
+        bCancel = (Button) findViewById(R.id.bCancel);
+        bSave = (Button) findViewById(R.id.bSave);
+
+        bCancel.setOnClickListener(this);
+        bSave.setOnClickListener(this);
+
     }
 
     @Override
@@ -55,14 +67,21 @@ public class Editar_Perfil extends AppCompatActivity {
         }
     }
 
-    public void onClick(View v){
-        if(v.getId() == R.id.bSave){
-            Log.d("CREATION", "Guardar is being executed!");
-            finish();
-            startActivity(new Intent(getApplicationContext(), Perfil .class));
+    public void onClick(View v) // botones
+    {
+        switch(v.getId())
+        {
+            case R.id.bSave:
+                finish();
+                startActivity(new Intent(getApplicationContext(), Perfil.class));
+                break;
+
+            case R.id.bCancel:
+                finish();
+                startActivity(new Intent(getApplicationContext(), Perfil.class));
+                break;
+
         }
-
     }
-
 
 }
