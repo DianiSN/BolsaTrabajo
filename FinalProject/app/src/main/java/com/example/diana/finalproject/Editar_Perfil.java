e6d74b27389b284d880c84f9ee674dcebf9f8d85
a01320622 <a01320622@itesm.mx>
2015-11-28 15:39:08 -0600

    Change profile pic
    NOT TESTED YET! Not tested yet but it lets the user pic an image from
    their gallery.

--- FinalProject/app/src/main/java/com/example/diana/finalproject/Editar_Perfil.java
+++ FinalProject/app/src/main/java/com/example/diana/finalproject/Editar_Perfil.java
@@ -1,6 +1,10 @@
 package com.example.diana.finalproject;
 
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +16,7 @@ import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -19,8 +24,11 @@ import android.widget.Toast;
  * Created by Andrea on 30/09/2015.
  */
 public class Editar_Perfil extends AppCompatActivity implements View.OnClickListener {
-    Button bCancel, bSave;
+    Button bCancel, bSave,bFoto;
+    ImageView imgPP;
     DataBaseHelper db;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,9 +37,13 @@ public class Editar_Perfil extends AppCompatActivity implements View.OnClickList
 
         bCancel = (Button) findViewById(R.id.bCancel);
         bSave = (Button) findViewById(R.id.bSave);
+        bFoto = (Button) findViewById(R.id.bfoto);
+        imgPP = (ImageView) findViewById(R.id.profilePic);
 
         bCancel.setOnClickListener(this);
         bSave.setOnClickListener(this);
+        bFoto.setOnClickListener(this);
+
 
     }
 
@@ -94,6 +106,51 @@ public class Editar_Perfil extends AppCompatActivity implements View.OnClickList
 //        }
     }
 
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+
+                // Set the Image in ImageView after decoding the String
+                imgPP.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
     public void onClick(View v) // botones
     {
         switch(v.getId())
@@ -107,6 +164,11 @@ public class Editar_Perfil extends AppCompatActivity implements View.OnClickList
             case R.id.bCancel:
                 finish();
                 startActivity(new Intent(getApplicationContext(), Perfil.class));
+                break;
+            case R.id.bfoto:
+                loadImagefromGallery(v);
+//              Toast.makeText(this, "Change pic", Toast.LENGTH_LONG).show();
+
                 break;
 
         }
